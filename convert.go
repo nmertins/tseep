1b306e876a491801779f505a9a70f0ed582839ef
Nathan Mertins <nwmertins@gmail.com>
2021-08-05 06:20:39 -0500

    function for parsing port number from hex

--- convert.go
+++ convert.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 )
@@ -8,8 +9,9 @@ import (
 type ConversionError string
 
 const (
-	HexCharactersIn32Bits    = 8
-	ErrInvalidCharacterCount = ConversionError("hexadecimal string does not have 8 characters")
+	HexCharactersInIPAddress = 8
+	HexCharactersInPort      = 4
+	MalformedHexInput        = ConversionError("hexadecimal string is malformed")
 )
 
 func (e ConversionError) Error() string {
@@ -19,12 +21,12 @@ func (e ConversionError) Error() string {
 // ConvertLittleEndianHexToIP returns the IPv4 address represented by the hexadecimal stirng s.
 //
 // ConvertLittleEndianHexToIP expects the following to be true:
-//   - string s to be EXACTLY 8  hexadecimal characters
+//   - string s to be EXACTLY 8 hexadecimal characters
 //   - string s to be formatted in little endian
-// If either is not true, an empty string is returned with an error.
+// If s is not of length 8 or contains non-hexadecimal characters, an empty string is returned with an error.
 func ConvertLittleEndianHexToIP(s string) (string, error) {
-	if len(s) != HexCharactersIn32Bits {
-		return "", ErrInvalidCharacterCount
+	if len(s) != HexCharactersInIPAddress {
+		return "", MalformedHexInput
 	}
 
 	octets, err := hex.DecodeString(s)
@@ -42,4 +44,25 @@ func ConvertLittleEndianHexToIP(s string) (string, error) {
 	}
 
 	return ip, nil
+}
+
+// ConvertBigEndianHexToPort returns the port represented by the hexadecimal stirng s.
+//
+// ConvertBigEndianHexToPort expects the following to be true:
+//   - string s to be EXACTLY 4 hexadecimal characters
+//   - string s to be formatted in big endian
+// If s is not of length 4 or contains non-hexadecimal characters, 0 will be returned with an error.
+func ConvertBigEndianHexToPort(s string) (uint16, error) {
+	if len(s) != HexCharactersInPort {
+		return 0, MalformedHexInput
+	}
+
+	portBytes, err := hex.DecodeString(s)
+	if err != nil {
+		return 0, MalformedHexInput
+	}
+
+	port := binary.BigEndian.Uint16(portBytes)
+
+	return port, nil
 }
--- convert_test.go
+++ convert_test.go
@@ -23,14 +23,59 @@ func TestConvertLittleEndianHexToIP(t *testing.T) {
 		}
 
 		tests := []test{
-			{hexInput: "01EE", want: ErrInvalidCharacterCount},
-			{hexInput: "", want: ErrInvalidCharacterCount},
+			{hexInput: "01EE", want: MalformedHexInput},
+			{hexInput: "", want: MalformedHexInput},
 		}
 
 		for _, tc := range tests {
 			_, err := ConvertLittleEndianHexToIP(tc.hexInput)
 			if err != tc.want {
 				t.Errorf("wanted an error but didn't get one")
+			}
+		}
+	})
+}
+
+func TestConvertBigEndianHexToPort(t *testing.T) {
+
+	t.Run("valid hex strings", func(t *testing.T) {
+		type test struct {
+			hexInput string
+			want     uint16
+		}
+
+		tests := []test{
+			{hexInput: "0050", want: 80},
+			{hexInput: "FFFF", want: 65535},
+			{hexInput: "01BB", want: 443},
+		}
+
+		for _, tc := range tests {
+			got, _ := ConvertBigEndianHexToPort(tc.hexInput)
+
+			if got != tc.want {
+				t.Errorf("got %d want %d", got, tc.want)
+			}
+		}
+	})
+
+	t.Run("invalid hex strings", func(t *testing.T) {
+		type test struct {
+			hexInput string
+			want     error
+		}
+
+		tests := []test{
+			{hexInput: "", want: MalformedHexInput},
+			{hexInput: "00", want: MalformedHexInput},
+			{hexInput: "TEST", want: MalformedHexInput},
+		}
+
+		for _, tc := range tests {
+			_, err := ConvertBigEndianHexToPort(tc.hexInput)
+
+			if err != tc.want {
+				t.Errorf("got %q want %q", err, tc.want)
 			}
 		}
 	})
